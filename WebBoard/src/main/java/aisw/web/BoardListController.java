110d4e5d926a7451b75e8be1c0d662acefbba6be
Lee Nayeon <in6306174@gmail.com>
2023-07-06 14:32:06 +0900

    spring @

--- WebBoard/src/main/java/aisw/web/BoardListController.java
+++ WebBoard/src/main/java/aisw/web/BoardListController.java
@@ -1,29 +1,48 @@
 package aisw.web;
 
+import java.sql.SQLException;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import aisw.web.biz.BoardDAO;
 import aisw.web.biz.BoardVO;
 
-public class BoardListController implements Controller {
+@Controller
+public class BoardListController {
+	
+	@Autowired
+	private BoardDAO boardDAO;
+//	
+//	public void setBoardDAO(BoardDAO boardDAO) {
+//	    this.boardDAO = boardDAO;
+//	}
+	
 
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-
-		BoardDAO boardDAO = new BoardDAO();
-		List<BoardVO> boardList = boardDAO.getBoardList();
-
-		ModelAndView mv = new ModelAndView();
-		mv.addObject("boardList", boardList);
-		mv.setViewName("boardList");
+	@RequestMapping("/boardList")
+	public String boardList(Model model) throws ClassNotFoundException, SQLException {
 		
-		return mv;
+		List<BoardVO> boardList = boardDAO.getBoardList();
+		model.addAttribute("boardList", boardList);
+		return "boardList";
 	}
+	
+
+//	@Override
+//	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+//
+//		List<BoardVO> boardList = boardDAO.getBoardList();
+//
+//		ModelAndView mv = new ModelAndView("boardList");
+//		mv.addObject("boardList", boardList);
+//		
+//		
+//		
+//		return mv;
+//	}
 
 }
--- WebBoard/src/main/java/aisw/web/biz/BoardDAO.java
+++ WebBoard/src/main/java/aisw/web/biz/BoardDAO.java
@@ -6,13 +6,28 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.sql.DataSource;
+import javax.websocket.server.ServerEndpoint;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
 import aisw.web.biz.JDBCUtil;
 
+@Service
 public class BoardDAO {
 
     private Connection conn;
     private PreparedStatement stmt;
     private ResultSet rs;
+    
+    @Autowired
+    private DataSource dataSource;
+//    
+//    public void setDataSource(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
 
     // 문의글 전체 보기
     public List<BoardVO> getBoardList() {
@@ -24,7 +39,8 @@ public class BoardDAO {
         sql.append(" ORDER BY B_NO DESC");
         sql.append("  ");
         try {
-            conn = JDBCUtil.getConnection();
+//            conn = JDBCUtil.getConnection();
+        	conn = dataSource.getConnection();
             stmt = conn.prepareStatement(sql.toString());
 
             rs = stmt.executeQuery();
@@ -43,30 +59,32 @@ public class BoardDAO {
         return boardList;
     }
 
-    // 문의글 하나보기
-    public BoardVO getBoard(int b_no) {
-        BoardVO board = null;
-        StringBuilder sql = new StringBuilder();
-
-        sql.append("SELECT * FROM BANK_BOARD WHERE B_NO = ?");
-        try {
-            conn = JDBCUtil.getConnection();
-            stmt = conn.prepareStatement(sql.toString());
-            stmt.setInt(1, b_no);
-
-            rs = stmt.executeQuery();
-
-            if (rs.next()) {
-                board = new BoardVO();
-                board.setB_no(rs.getInt("B_NO"));
-                board.setTitle(rs.getString("TITLE"));
-                board.setContent(rs.getString("CONTENT"));
-                board.setReg_date(rs.getDate("REG_DATE"));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return board;
-    }
+//    // 문의글 하나보기
+//    public BoardVO getBoard(int b_no) {
+//        BoardVO board = null;
+//        StringBuilder sql = new StringBuilder();
+//
+//        sql.append("SELECT * FROM BANK_BOARD WHERE B_NO = ?");
+//        try {
+//            conn = JDBCUtil.getConnection();
+    
+//            conn = dataSource.getConnection();	
+//            stmt = conn.prepareStatement(sql.toString());
+//            stmt.setInt(1, b_no);
+//
+//            rs = stmt.executeQuery();
+//
+//            if (rs.next()) {
+//                board = new BoardVO();
+//                board.setB_no(rs.getInt("B_NO"));
+//                board.setTitle(rs.getString("TITLE"));
+//                board.setContent(rs.getString("CONTENT"));
+//                board.setReg_date(rs.getDate("REG_DATE"));
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return board;
+//    }
 
 }
