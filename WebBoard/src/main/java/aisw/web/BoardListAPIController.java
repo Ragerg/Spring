331763cf76c4ae93e05616d28f753ee4b94f413c
Lee Nayeon <in6306174@gmail.com>
2023-07-07 15:28:45 +0900

    Rest API : ResponseEntity 없이 할 수 있게 수정

--- WebBoard/src/main/java/aisw/web/BoardListAPIController.java
+++ WebBoard/src/main/java/aisw/web/BoardListAPIController.java
@@ -26,62 +26,61 @@ public class BoardListAPIController {
 
 	//API : 전체목록
 	@GetMapping("/api/news")
-	public ResponseEntity<List<BoardVO>> boardListAPI() throws ClassNotFoundException, SQLException {
+	public List<BoardVO> boardListAPI() throws ClassNotFoundException, SQLException {
 		
 		List<BoardVO> boardList = boardDAO.getBoardList();
-		return ResponseEntity.ok(boardList);
+		return boardList;
 	}
 	
 	// API: 개별 게시글
 	@GetMapping("/api/news/{b_no}")
-	public ResponseEntity<BoardVO> boardAPI(@PathVariable Integer b_no) throws ClassNotFoundException, SQLException {
+	public BoardVO boardAPI(@PathVariable Integer b_no) throws ClassNotFoundException, SQLException {
 		
 		// 조회수 증가
 		boardDAO.increaseHitCount(b_no);
 		
 		BoardVO board = boardDAO.getBoard(b_no);
-		return ResponseEntity.ok(board);
+		return board;
 	}
 	
 	// API: 글 등록
 	@PostMapping("/api/news")
-	public ResponseEntity<String> insertBoardAPI(BoardVO board) throws ClassNotFoundException, SQLException {
+	public String insertBoardAPI(BoardVO board) throws ClassNotFoundException, SQLException {
 		
         int result = boardDAO.insertBoard(board);
 
         if (result == 1) {
-            return ResponseEntity.ok("공지가 등록되었습니다.");
+            return "공지가 등록되었습니다.";
         } else {
-            return ResponseEntity.status(500).body("공지 등록에 실패하였습니다. 잠시 후 다시 시도해주세요.");
+            return "공지 등록에 실패하였습니다. 잠시 후 다시 시도해주세요.";
         }
 	}
 	
 	// API: 글 삭제
-	@DeleteMapping(value="/api/news/{b_no}", produces = "text/json; charset=UTF-8")
-	public ResponseEntity<String> deleteBoardAPI(@PathVariable("b_no") Integer b_no) throws ClassNotFoundException, SQLException {
+	@DeleteMapping("/api/news/{b_no}")
+	public String deleteBoardAPI(@PathVariable("b_no") Integer b_no) throws ClassNotFoundException, SQLException {
 
 	    int result = boardDAO.deleteBoard(b_no);
 
 	    if (result == 1) {
-            return ResponseEntity.ok().body("{\"message\": \"게시글이 삭제되었습니다.\"}");
+            return "게시글이 삭제되었습니다.";
         } else {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("{\"message\": \"게시글 삭제에 실패하였습니다. 잠시 후 다시 시도해주세요.\"}");
+            return "게시글 삭제에 실패하였습니다. 잠시 후 다시 시도해주세요.";
         }
     }
 	
 	// API: 글 수정
-	@PostMapping(value="/api/news/{b_no}", produces = "text/json; charset=UTF-8")
-	public ResponseEntity<String> updateBoardAPI(BoardVO board, @PathVariable("b_no") Integer b_no) throws ClassNotFoundException, SQLException {
+	@PostMapping("/api/news/{b_no}")
+	public String updateBoardAPI(BoardVO board, @PathVariable("b_no") Integer b_no) throws ClassNotFoundException, SQLException {
 
 		board.setB_no(b_no);
 		
 	    int result = boardDAO.updateBoard(board);
 
 	    if (result == 1) {
-	        return ResponseEntity.ok("게시글이 수정되었습니다.");
+	        return "게시글이 수정되었습니다.";
 	    } else {
-	        return ResponseEntity.status(500).body("게시글 수정에 실패하였습니다. 잠시 후 다시 시도해주세요.");
+	        return "게시글 수정에 실패하였습니다. 잠시 후 다시 시도해주세요.";
 	    }
 	}
 
