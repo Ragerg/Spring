dc4ad4068a42ba8d19c05547110512d8f7269d09
Lee Nayeon <in6306174@gmail.com>
2023-07-07 09:23:00 +0900

    게시판 새 sql

---
+++ WebBoard/NEWSBOARD.sql
@@ -0,0 +1,27 @@
+CREATE SEQUENCE b_seq
+  START WITH 1
+  INCREMENT BY 1
+  NOCACHE
+  NOCYCLE;
+
+CREATE TABLE NEWS_BOARD (
+  B_NO NUMBER DEFAULT b_seq.NEXTVAL PRIMARY KEY,
+  TITLE VARCHAR2(200) NOT NULL,
+  USER_ID VARCHAR2(20) NOT NULL,
+  CONTENT CLOB NOT NULL,
+  REG_DATE DATE DEFAULT SYSDATE,
+  HIT NUMBER,
+  IMAGE CLOB
+);
+
+INSERT INTO NEWS_BOARD (B_NO, TITLE, USER_ID, CONTENT, HIT, IMAGE)
+VALUES (1, '제목 예제', 'user1', '내용 예제', 0, 'https://example.com/image1.jpg');
+
+INSERT INTO NEWS_BOARD (B_NO, TITLE, USER_ID, CONTENT, HIT, IMAGE)
+VALUES (2, '또 다른 제목', 'user2', '또 다른 내용', 0, 'https://example.com/image2.jpg');
+
+SELECT * FROM NEWS_BOARD;
+
+delete from news_board where b_no=3 or b_no=4 or b_no=5 or b_no=7;
+
+COMMIT;
